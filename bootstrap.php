915dc81670a19c4c60b9920d88f5ee6c8ebdf0f2
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-11-09 17:17:40 -0300

    config environment

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,8 @@
 require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use Src\Databases\DatabaseConnection;
 
-$dotenv = Dotenv::createImmutable(__DIR__)->load();
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$databaseConnection = (new DatabaseConnection())->getConnection();
