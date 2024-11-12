704c28b202febbe8dcb080205b76fd16be6be592
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-11-11 21:29:28 -0300

    refactory get content codes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Src\Controllers\AuthorsControllers;
 use Src\Controllers\MoviesControllers;
 
 require '../bootstrap.php';
@@ -12,29 +13,24 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
-$movieId  = null;
+
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-$controller    = (new MoviesControllers($databaseConnection, $requestMethod, $movieId))->processRequest();
-$jsonfyController = json_decode($controller, true);
 
-
-function getMoviesContentByUrl(array $uri, $movieId, $jsonfyController) {
-  if ($uri[1] === "") {
-    return json_encode([
-      "API" => "locadora_php",
-      "Owner" => "Rafael Citario",
-      "Urls" => json_encode(["/movies", "movies/<some number>"], true)
-    ], true);
-  }
-  if ($uri[1] !== 'movies') {
-    header("HTTP/1.1 404 Not Found!");
-    return 'This page not exists!';
-  }
-
+$movieId = null;
+$authorId = null;
+if ($uri[1] === 'movies') {
   if (isset($uri[2])) {
     $movieId = (int) $uri[2];
-    return json_encode($jsonfyController[$movieId - 1], true);
   }
-  return json_encode($jsonfyController, true);
+  $moviesControllers = (new MoviesControllers($databaseConnection, $requestMethod, $movieId))->processRequest();
+  $encodedMovies     = json_encode($moviesControllers);
+  echo json_decode($encodedMovies, true);
 }
-echo getMoviesContentByUrl($uri, $movieId, $jsonfyController);
+if ($uri[1] === "authors") {
+  if (isset($uri[2])) {
+    $authorId = (int) $uri[2];
+  }
+  $authorsControllers = (new AuthorsControllers($databaseConnection, $requestMethod, $authorId))->processRequest();
+  $encodedAuthors     = json_encode($authorsControllers);
+  echo json_decode($encodedAuthors, true);
+}
